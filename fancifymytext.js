c8a53d4dfcac72c331d4cdabd371e127eb234ace
antonyus <51768491+antonyus@users.noreply.github.com>
2021-03-14 18:57:11 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,27 @@
+alert("Hello, world!");
+
+function fancyschamncy() 
+{
+    document.getElementById("text").style.fontWeight = 'bold';
+    document.getElementById("text").style.color = "blue";
+    document.getElementById("text").style.textDecoration = "underline dotted";
+}
+function boringbetty() 
+{
+    document.getElementById("text").style.fontWeight = 'initial';
+    document.getElementById("text").style.color = "initial";
+    document.getElementById("text").style.textDecoration = "initial";
+}
+
+function bigger() 
+{
+    document.getElementById('text').style.fontSize = '24em';
+} 
+
+function moo() 
+{
+    document.getElementById('text').style.textTransform = 'uppercase'
+    var str = document.getElementById('text').value;
+    var parts = str.split(".");
+    str = parts.join("moo");
+}
